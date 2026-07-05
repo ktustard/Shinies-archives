cb671be752a3f683bf93bd860eba32ffb4be171e
Innah Alagao <alagao.dev@gmail.com@example.com>
2026-07-06 07:39:15 +0800

    timeline-json

---
+++ js/timeline.js
@@ -0,0 +1,64 @@
+[
+    {
+        "year":"2018",
+        "title":"Pinoy Big Brother: Otso",
+        "image":"images/timeline/pbb.jpg",
+        "description":"Sheena Catacutan first gained public attention as a contestant on Pinoy Big Brother: Otso."
+    },
+
+    {
+        "year":"2019",
+        "title":"Star Hunt Academy",
+        "image":"images/timeline/sha.jpg",
+        "description":"Joined the Star Hunt Academy and began intensive idol training."
+    },
+
+    {
+        "year":"2020",
+        "title":"Official BINI Member",
+        "image":"images/timeline/2020.jpg",
+        "description":"Graduated from SHA and introduced as the eighth member of BINI. Released Da Coconut Nut."
+    },
+
+    {
+        "year":"2021",
+        "title":"Born To Win Era",
+        "image":"images/timeline/2021.jpg",
+        "description":"Official debut with Born To Win and release of the group's first studio album."
+    },
+
+    {
+        "year":"2022",
+        "title":"Feel Good Era",
+        "image":"images/timeline/2022.jpg",
+        "description":"Released Feel Good featuring I Feel Good and Lagi."
+    },
+
+    {
+        "year":"2023",
+        "title":"Pantropiko Era",
+        "image":"images/timeline/2023.jpg",
+        "description":"Karera and Pantropiko became massive hits across the Philippines."
+    },
+
+    {
+        "year":"2024",
+        "title":"Talaarawan Era",
+        "image":"images/timeline/2024.jpg",
+        "description":"Released Talaarawan EP, Salamin Salamin and Cherry On Top."
+    },
+
+    {
+        "year":"2025",
+        "title":"FLAMES Era",
+        "image":"images/timeline/2025.jpg",
+        "description":"Reached one billion Spotify streams. Released Blink Twice and FLAMES."
+    },
+
+    {
+        "year":"2026",
+        "title":"Coachella & Summer Sonic",
+        "image":"images/timeline/2026.jpg",
+        "description":"First Filipino act to perform at Coachella and later performed at Summer Sonic Japan."
+    }
+]
