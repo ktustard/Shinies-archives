fecdb45e86886a4c9d7ddbd22f362dcea054b428
Innah Alagao <alagao.dev@gmail.com@example.com>
2026-07-07 13:15:13 +0800

    mobile

--- js/navbar.js
+++ js/navbar.js
@@ -1,44 +1,35 @@
 const sections = document.querySelectorAll("section");
-const navLinks = document.querySelectorAll(".nav-links a");
+const navAnchorLinks = document.querySelectorAll(".nav-links a");
 
 window.addEventListener("scroll", () => {
-
     let current = "";
 
     sections.forEach(section => {
-
         const sectionTop = section.offsetTop - 120;
-        const sectionHeight = section.offsetHeight;
 
         if (window.scrollY >= sectionTop) {
             current = section.getAttribute("id");
         }
-
     });
 
-    navLinks.forEach(link => {
-
+    navAnchorLinks.forEach(link => {
         link.classList.remove("active");
 
-        if(link.getAttribute("href") === "#" + current){
-
+        if (link.getAttribute("href") === "#" + current) {
             link.classList.add("active");
-
         }
-
     });
-
 });
 
 const menuToggle = document.getElementById("menuToggle");
-const navLinks = document.getElementById("navLinks");
+const navMenu = document.getElementById("navLinks");
 
 menuToggle.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
+    navMenu.classList.toggle("active");
 });
 
-navLinks.querySelectorAll("a").forEach(link => {
+navMenu.querySelectorAll("a").forEach(link => {
     link.addEventListener("click", () => {
-        navLinks.classList.remove("active");
+        navMenu.classList.remove("active");
     });
 });
